154f0410ac4f3adaa80e529cbd2afa0a0c1d3587
joaoaziliero <146762748+joaoaziliero@users.noreply.github.com>
2024-11-17 23:40:20 -0300

    create asset processing oriented script template

---
+++ Assets/Scripts/Tests/CustomBehaviourScript.cs
@@ -0,0 +1,23 @@
+using static Core.Utils.GameLogicManagement.AssetProcessing;
+using Core.Utils.NamingConventions;
+using System.Linq;
+using UnityEngine;
+
+public class CustomBehaviourScript : MonoBehaviour
+{
+    private void Start()
+    {
+        StartCoroutine(DeployAssets(LoadAssetsAsync<UnityEngine.Object>(AssetGroupLabel.Default), handle =>
+        {
+            var assets = handle.Result.OrderBy(asset => asset.name);
+            // Your code here
+            handle.ReleaseOnDestroy(this);
+        }
+        ));
+    }
+
+    private void OnDestroy()
+    {
+        StopAllCoroutines();
+    }
+}
